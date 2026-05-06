24b2de6b54d65e381e060067a03868a0383dd37c
geracodebr <geracodebr@gmail.com>
2026-05-06 16:56:05 -0300

    array
    preciso treinar mais, nao entendi nada

---
+++ script.js
@@ -0,0 +1,8 @@
+const convidados = ['Sheila', 'Cleyton', 'Leticia'];
+convidados.push("André");
+console.log(convidados.length);
+console.log(convidados[convidados.length -1]);
+convidados[convidados.length] = "Paulo";
+console.log(convidados);
+
+
